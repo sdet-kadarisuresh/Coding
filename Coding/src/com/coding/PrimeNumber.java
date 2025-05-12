2a6e5eb69281da43dc727242c5e8d64e6f80dd11
sdet-kadarisuresh <kadarisureshsdet@gmail.com>
2025-05-12 12:23:43 +0530

    Update PrimeNumber.java

--- Coding/src/com/coding/PrimeNumber.java
+++ Coding/src/com/coding/PrimeNumber.java
@@ -1,17 +1,18 @@
 package com.coding;
 
-public class PrimeNumber {
+public class PrimeNumbers {
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++) {
             boolean isPrime = true;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
+
+            for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
                 }
             }
             if (isPrime) {
-                System.out.print(i + " ");
+                System.out.println(i + " is a prime number");
             }
         }
     }
